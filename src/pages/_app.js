53a2e227a65619b6eed38e933946b680ed3f677e
ronny-minda <ronny.minda.vera@gmail.com>
2023-04-07 10:08:15 -0600

    connction

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
+import { ApolloClient, InMemoryCache, HttpLink, ApolloProvider } from '@apollo/client'
 import '@/styles/globals.css'
 
+const client = new ApolloClient({
+  cache: new InMemoryCache,
+  link: new HttpLink({
+    uri: 'http://localhost:3000/api/graphql'
+  })  
+})
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
--- src/pages/api/graphql.js
+++ src/pages/api/graphql.js
@@ -1,18 +1,84 @@
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { gql } from 'graphql-tag';
+import { v1 as uuid } from 'uuid'
+
+const personas = [
+  {
+    id: 1,
+    nombre: "Juan",
+    telefono: "555-1234",
+    ciudad: "Ciudad de México"
+  },
+  {
+    id: 2,
+    nombre: "Ana",
+    telefono: "555-5678",
+    ciudad: "Guadalajara"
+  },
+  {
+    id: 3,
+    nombre: "Pedro",
+    telefono: "555-9012",
+    ciudad: "Monterrey"
+  },
+  {
+    id: 4,
+    nombre: "María",
+    telefono: "555-3456",
+    ciudad: "Puebla"
+  },
+  {
+    id: 5,
+    nombre: "Luis",
+    telefono: "555-7890",
+    ciudad: "Tijuana"
+  }
+];
+
+
+
+const typeDefs = gql`
+  type Person {
+    id: String!
+    nombre: String!
+    telefono: String
+    ciudad: String
+  }
+
+  type Query {
+    personCount: Int!
+    allPerson: [Person]!
+    numeroPersonas(numero: Int!): [Person]
+  }
+
+  type Mutation {
+    addPerson(
+      nombre: String!
+      telefono: String!
+      ciudad: String!
+    ): Person
+  }
+`;
 
 const resolvers = {
   Query: {
-    hello: () => 'world',
+    personCount: () => personas.length,
+    allPerson: () => personas,
+    numeroPersonas: (root, args) => {
+      const {numero} = args
+      return personas.slice(0, numero)
+    }
   },
-};
-
-const typeDefs = gql`
-  type Query {
-    hello: String
+  Mutation: {
+    addPerson: (root, args) => {
+      const persona = {...args, id: uuid()}
+      personas.push(persona)
+      console.log(persona)
+      return persona
+    }
   }
-`;
+};
 
 const server = new ApolloServer({
   resolvers,
